7b5a64682963738ddfeaea977c3c477f4065764c
kaerhae <erno.karha@hotmail.com>
2024-08-07 21:22:38 +0300

    Fixed user login and signup to case-insensitive; Added unit tests; Added XML comments

--- dotnet-voyage-log/Utilities/Authentication.cs
+++ dotnet-voyage-log/Utilities/Authentication.cs
@@ -4,7 +4,9 @@ using Newtonsoft.Json;
 using BC = BCrypt.Net.BCrypt;
 
 namespace dotnet_voyage_log.Utilities;
-
+/// <summary>
+/// Authentication. Constructor requires IUserRepository.
+/// </summary>
 public class Authentication : IAuthentication
 {
 
@@ -14,12 +16,24 @@ public class Authentication : IAuthentication
         _userRepository = userRepository;
     }
 
+    /// <summary>
+    /// Password hash function.
+    /// </summary>
+    /// <returns>
+    /// string: Hashed password
+    /// </returns>
     public string  HashPassword(string plain)
     {
         string hash = BC.HashPassword(plain);
         return hash;
     }
 
+    /// <summary>
+    /// Check that plain password is valid by comparing hash from database.
+    /// </summary>
+    /// <returns>
+    /// True or false
+    /// </returns>
     public bool IsValidPassword(string hash, string plain)
     {
         bool isAuthenticated = BC.Verify(plain, hash);
@@ -30,6 +44,14 @@ public class Authentication : IAuthentication
         return true;
     }
 
+    /// <summary>
+    /// Checks that user is the owner of Voyage object. Is checking that Voyage object contains userId.
+    /// </summary>
+    /// <returns>
+    /// True or false
+    /// </returns>
+    /// /// <exception cref="UnauthorizedAccessException">
+    /// </exception>
     public bool IsOwner(long userId, Voyage voyage) {
         User? user = _userRepository.RetrieveSingleUserById(userId);
         System.Diagnostics.Debug.WriteLine(JsonConvert.SerializeObject(user, Formatting.Indented));
